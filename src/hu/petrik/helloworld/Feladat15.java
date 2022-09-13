e78f3170169aca27bdaeb0a289110e966bdb8428
PetrikDiak <petrikdiak@gmail.com>
2022-09-13 16:27:05 +0200

    15.  kesz

--- src/hu/petrik/helloworld/Feladat15.java
+++ src/hu/petrik/helloworld/Feladat15.java
@@ -3,5 +3,11 @@ package hu.petrik.helloworld;
 public class Feladat15 {
     public static void main(String[] args) {
         double[] tomb = new double[25];
+        for (int i = 0; i < tomb.length; i++) {
+            tomb[i]=Math.random()*200-100;
+        }
+        for (int i = 0; i < tomb.length; i+=2) {
+            System.out.println((tomb[i]*tomb[i])+ " ");
+        }
     }
 }
